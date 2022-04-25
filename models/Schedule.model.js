5aa7bdc75a0f65444d10eafb95795158da594d3e
SD170 <duttasaswata7@gmail.com>
2022-04-25 16:24:06 +0530

    Schedule model added

---
+++ models/Schedule.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ScheduleSchema = new mongoose.Schema(
+  {
+    hostUserId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Please add userId"],
+    },
+    guestUsers: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+    },
+    roomId: {
+      type: String,
+      required: [true, "Please add roomId"],
+    },
+    meetingDate: {
+      type: Date,
+    },
+    startTime: {
+      type: Date,
+    },
+    endTime: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Schedule", ScheduleSchema);
